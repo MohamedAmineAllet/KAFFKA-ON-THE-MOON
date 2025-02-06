de6d3e1792dbfd745394f6fd9d122ec992fc488c
kian <kiangokh2@gmail.com>
2025-02-06 12:57:01 -0500

    J'ai essayer de coder un peu le decollage du drone dans main_SoftwareInTheLoop.py
    Date :  2025-02-03
    commit par : Kian

--- Hardware/main_SoftwareInTheLoop.py
+++ Hardware/main_SoftwareInTheLoop.py
@@ -1,4 +1,5 @@
 import dronekit_sitl
+import time
 from dronekit  import connect, VehicleMode
 from collections.abc import MutableMapping
 
@@ -6,6 +7,28 @@ from collections.abc import MutableMapping
 sitl = dronekit_sitl.start_default()
 connection_string = sitl.connection_string()
 vehicle = connect(connection_string, wait_ready=True) #Se connecter au véhicule simulé
+
+def  arm_and_takeoff(target_altitude):
+    print("Arme le drone en mode GUIDED")
+    vehicle.mode = VehicleMode("GUIDED")
+    while vehicle.mode.name != "GUIDED":
+        time.sleep(1)
+
+        print("Armement des moteurs...")
+
+        vehicle.armed = True
+
+    while not vehicle.armed:
+        time.sleep(1)
+
+    print("Décollage en cours...")
+    vehicle.simple_takeoff(target_altitude)
+
+
+arm_and_takeoff(10)
+
+print("Atterrissage automatique...")
+vehicle.mode = VehicleMode("LAND")
 
 
 print(f"Mode: {vehicle.mode.name}")
