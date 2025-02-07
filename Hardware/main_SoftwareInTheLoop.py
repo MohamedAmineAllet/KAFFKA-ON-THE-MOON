9e45a9c2dc8e7a7934c26ceb324da8e36c7911d8
KKML19 <6224633@bdeb.qc.ca>
2025-02-06 22:36:07 -0500

    Permettre de run séparément sitl et main application

--- Hardware/main_SoftwareInTheLoop.py
+++ Hardware/main_SoftwareInTheLoop.py
@@ -1,35 +1,30 @@
 import dronekit_sitl
 import time
-from dronekit  import connect, VehicleMode
-from collections.abc import MutableMapping
+from dronekit import connect, VehicleMode
 
 # Commencer SITL
 sitl = dronekit_sitl.start_default()
 connection_string = sitl.connection_string()
-vehicle = connect(connection_string, wait_ready=True) #Se connecter au véhicule simulé
+vehicle = connect(connection_string, wait_ready=True)  # Se connecter au véhicule simulé
 
-def  arm_and_takeoff(target_altitude):
+
+def arm_and_takeoff(target_altitude):
     print("Arme le drone en mode GUIDED")
     vehicle.mode = VehicleMode("GUIDED")
     while vehicle.mode.name != "GUIDED":
         time.sleep(1)
-
-
 
         vehicle.armed = True
 
     while not vehicle.armed:
         time.sleep(1)
 
-
     vehicle.simple_takeoff(target_altitude)
 
 
 arm_and_takeoff(10)
 
-
 vehicle.mode = VehicleMode("LAND")
-
 
 print(f"Mode: {vehicle.mode.name}")
 
