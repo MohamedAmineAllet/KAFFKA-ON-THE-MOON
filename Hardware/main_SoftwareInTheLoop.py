9005b5377f2d185cbcc65cc1c8041751fdb2c442
kian <kiangokh2@gmail.com>
2025-02-10 15:59:37 -0500

    J'ai essayer de coder un peu le decollage du drone dans main_SoftwareInTheLoop.py
    Date :  2025-02-03
    commit par : Kian

--- Hardware/main_SoftwareInTheLoop.py
+++ Hardware/main_SoftwareInTheLoop.py
@@ -1,5 +1,7 @@
 import dronekit_sitl
 import time
+import argparse
+import typing_extensions
 from dronekit import connect, VehicleMode
 
 # Commencer SITL
@@ -8,8 +10,20 @@ connection_string = sitl.connection_string()
 vehicle = connect(connection_string, wait_ready=True)  # Se connecter au véhicule simulé
 
 
+def connectMyCopter():
+    parser = argparse.ArgumentParser(description="commands")
+    parser.add_argument("--connect")
+    args = parser.parse_args()
+    connection_string = args.connect
+    vehicle = connect(connection_string, wait_ready=True)
+    return vehicle
+
+
 def arm_and_takeoff(target_altitude):
-    print("Arme le drone en mode GUIDED")
+    while not vehicle.is_armable:
+        print("wait for vehicle to be armed")
+        time.sleep(1)
+
     vehicle.mode = VehicleMode("GUIDED")
     while vehicle.mode.name != "GUIDED":
         time.sleep(1)
@@ -21,13 +35,27 @@ def arm_and_takeoff(target_altitude):
 
     vehicle.simple_takeoff(target_altitude)
 
+    while True:
+        print("Current Altitude: %d" % vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt > target_altitude:
+            break
+        time.sleep(1)
+    print("Target altitude reached")
+    return None
 
-arm_and_takeoff(10)
 
+vehicle = connectMyCopter()
+vehicle.mode = VehicleMode("GUIDED")
+arm_and_takeoff(2)
 vehicle.mode = VehicleMode("LAND")
+time.sleep(2)
 
-print(f"Mode: {vehicle.mode.name}")
+while True:
+    time.sleep(2)
+    vehicle.close()
+    sitl.stop()
+#print(f"Mode: {vehicle.mode.name}")
 
 # Fermer SITL proprement
-vehicle.close()
-sitl.stop()
+
+
