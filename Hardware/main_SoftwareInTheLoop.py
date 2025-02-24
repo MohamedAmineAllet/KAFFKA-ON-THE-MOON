f18927090154f5a7063f226448c55f782f28567a
KKML19 <6224633@bdeb.qc.ca>
2025-02-24 18:20:26 -0500

    Comment connecter Pycharm et software in the loop enfin!

--- Hardware/main_SoftwareInTheLoop.py
+++ Hardware/main_SoftwareInTheLoop.py
@@ -1,30 +1,42 @@
+import subprocess
 import dronekit_sitl
-import time
 import argparse
-
 import collections
+
 if not hasattr(collections, 'MutableMapping'):
     import collections.abc
+
     collections.MutableMapping = collections.abc.MutableMapping
 
-
 from dronekit import connect, VehicleMode
+import time
+
 
 # Commencer SITL
 sitl = dronekit_sitl.start_default()
-connection_string = sitl.connection_string()
+connection_string = 'tcp:127.0.0.1:5760'  # Port ouvert par SITL
+#connection_string = 'tcp:127.0.0.1:5762'  # Port ouvert par SITL
 
-print(f"Connect Mission Planner to: {connection_string}")  # Print the connection strin
+print(f"Connecting to vehicle on: {connection_string}")
+
+"""# 2 Lancer Mission Planner automatiquement
+mission_planner_path = "C:\\Program Files (x86)\\Mission Planner\\MissionPlanner.exe"
+subprocess.Popen([mission_planner_path])
+# Attendre que Mission Planner se lance
+time.sleep(5) """
+
+# connecter dronekit
 vehicle = connect(connection_string, wait_ready=True)  # Se connecter au véhicule simulé
+time.sleep(5)
 
-
+"""# connect to Firmware soit FC et ardupilot
 def connectMyCopter():
     parser = argparse.ArgumentParser(description="commands")
     parser.add_argument("--connect", default=connection_string)
     args = parser.parse_args()
     vehicle = connect(connection_string, wait_ready=True)
     return vehicle
-
+"""
 
 def arm_and_takeoff(target_altitude):
     while not vehicle.is_armable:
@@ -34,11 +46,13 @@ def arm_and_takeoff(target_altitude):
     vehicle.mode = VehicleMode("GUIDED")
     while vehicle.mode.name != "GUIDED":
         time.sleep(1)
-
         vehicle.armed = True
+    print("Mode guided")
 
     while not vehicle.armed:
         time.sleep(1)
+    print("Vehicle armed")
+
 
     vehicle.simple_takeoff(target_altitude)
 
@@ -51,7 +65,7 @@ def arm_and_takeoff(target_altitude):
     return None
 
 
-vehicle = connectMyCopter()
+#vehicle = connectMyCopter()
 vehicle.mode = VehicleMode("GUIDED")
 arm_and_takeoff(2)
 vehicle.mode = VehicleMode("LAND")
@@ -62,8 +76,6 @@ while True:
 
 vehicle.close()
 sitl.stop()
-#print(f"Mode: {vehicle.mode.name}")
+# print(f"Mode: {vehicle.mode.name}")
 
 # Fermer SITL proprement
-
-
