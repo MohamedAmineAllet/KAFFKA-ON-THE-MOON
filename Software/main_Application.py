574118dfd932cf93aa71709f47dabc448dc4456a
MohamedAmineAllet <157835866+MohamedAmineAllet@users.noreply.github.com>
2025-03-03 20:26:29 -0500

    1)Ajout d'un bouton activation/desactivaton camera. 2)Ajout bouton retour. Date :  2025-03-03 commit par : Mohamed-Amine,Allet

--- Software/main_Application.py
+++ Software/main_Application.py
@@ -2,7 +2,8 @@ from kivy.app import App
 from kivy.uix.image import Image
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition, FadeTransition, RiseInTransition, \
+    SwapTransition
 import cv2
 #Biblioteque utile a la transmission video du Rpy vers l'appareil.
 
@@ -10,31 +11,58 @@ import cv2
 class CameraWidget(Image):
     def __init__(self, **kwargs):
         super(CameraWidget, self).__init__(**kwargs)
-        # Ouvre la capture de la webcam
-        #self.capture = cv2.VideoCapture('http://10.186.13.54:4747/video')
-        self.capture = cv2.VideoCapture(0)
+        self.capture = None  # La capture vidéo sera activée/désactivée
+
+#a changer la source pour l'URL de la camera.
+    def start_camera(self,source=0, fps=30):
+        #Demarre la camera dependamment de la source.
+        self.capture = cv2.VideoCapture(source)
+
         if not self.capture.isOpened():
-            print("Erreur d'ouverture du flux de la webcam.")
-        # Planifie l'update à 30 fps
-        Clock.schedule_interval(self.update, 1.0/30.0)
+            print("Erreur : Impossible d'ouvrir la caméra.")
+            return
+        Clock.schedule_interval(self.update, 1.0 / fps)
 
     def update(self, dt):
-        ret, frame = self.capture.read()
-        if ret:
-            # Ici, on effectue éventuellement un flip (adaptable selon tes besoins)
-            buf = cv2.flip(frame, 0).tobytes()
-            texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-            texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-            self.texture = texture
+
+        if self.capture:
+            ret, frame = self.capture.read()
+            if ret:
+                buf = cv2.flip(frame, 0).tobytes()
+                texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+                texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+                self.texture = texture
+
+    def stop_camera(self):
+        #Arrete la transmission video et remet l'image par defaut
+        if self.capture:
+            self.capture.release()
+            self.capture = None
+        Clock.unschedule(self.update)
+        self.source = "ImageInterfaceCamera/ImageArriereFondCamera.png"
 
 class InterfaceDAcceuil(Screen):
     pass
 class InterfacePilotage(Screen):
-    pass
-    def press(self):
+    camera_active = False #Etat de la camera
+
+    def connecterLaCamera(self):
+
+        camera = self.ids.camera_widget
+        if not self.camera_active:
+            # Active la caméra
+            camera.start_camera(0)
+            self.ids.btn_toggle.text = "Désactiver la caméra"
+        else:
+            # Désactive la caméra
+            camera.stop_camera()
+            self.ids.btn_toggle.text = "Activer la caméra"
+            camera.texture = Image(source="ImageInterfaceCamera/ImageArriereFondCamera.png").texture
+        self.camera_active = not self.camera_active
+
+    def demarrerHandTracking(self):
+        #Ce code nous permet de lancer un autre fichier python dans le fichier python courrant.
         import os
-
-
         def run_program():
             os.system('python Simple-Hand-Tracker.py')
 
@@ -42,7 +70,11 @@ class InterfacePilotage(Screen):
 
 class CameraProjetApp(App):
     def build(self):
-        return ScreenManager()
+        sm = ScreenManager()
+        #A decider quelle transition choisir est la meilleur.
+        #Permet de choisir le type de transition.
+        sm.transition = SwapTransition()
+        return sm
 
 
 if __name__ == '__main__':
