b478b5cffff60378e57f83043dc63b384222e74f
kian <kiangokh2@gmail.com>
2025-03-05 21:47:04 -0500

    1)Ajout couleur et contraste du text app kivy + test de l'accesibilité d'un fichier python dans un autre fichier python. 2) J'ai fini le nettoyage des bases de données. 3) J'ai commence la construction & l'entrainement de du ml.(j'ai creer 2 type de ml) et je compte en creer un 3iem plus tard. Date :  2025-02-13 commit par : Mohamed-Amine,Allet

--- Hardware/main_SoftwareInTheLoop.py
+++ Hardware/main_SoftwareInTheLoop.py
@@ -3,12 +3,16 @@ import time
 import argparse
 
 import collections
+
+from dronekit_sitl.pysim.sim_wrapper import counter
+
 if not hasattr(collections, 'MutableMapping'):
     import collections.abc
     collections.MutableMapping = collections.abc.MutableMapping
 
 
 from dronekit import connect, VehicleMode
+from pymavlink import mavutil
 
 # Commencer SITL
 sitl = dronekit_sitl.start_default()
@@ -24,6 +28,18 @@ def connectMyCopter():
     args = parser.parse_args()
     vehicle = connect(connection_string, wait_ready=True)
     return vehicle
+
+def set_velocity_body(vx, vy, vz):
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,
+        0,0,
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
+        0B0000111111000111,
+        vx,vy,vz,
+        0,0,0,
+        0,0,)
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
 
 
 def arm_and_takeoff(target_altitude):
@@ -53,8 +69,53 @@ def arm_and_takeoff(target_altitude):
 
 vehicle = connectMyCopter()
 vehicle.mode = VehicleMode("GUIDED")
-arm_and_takeoff(2)
-vehicle.mode = VehicleMode("LAND")
+arm_and_takeoff(4)
+counter = 0
+while counter < 2:
+    set_velocity_body(1, 0, 0)
+    print("direction Nord")
+    time.sleep(1)
+    counter = counter + 1
+#negative sud, positif nord
+time.sleep(1)
+counter = 0
+
+while counter < 2:
+    set_velocity_body(-1, 0, 0)
+    print("direction Sud")
+    time.sleep(1)
+    counter = counter + 1
+
+time.sleep(1)
+counter = 0
+#negative ouest, positif est
+while counter < 2:
+    set_velocity_body(0, 1, 0)
+    print("direction est")
+    time.sleep(1)
+    counter = counter + 1
+
+time.sleep(1)
+counter = 0
+
+while counter < 2:
+    set_velocity_body(0, -1, 0)
+    print("direction ouest")
+    time.sleep(1)
+    counter = counter + 1
+
+time.sleep(1)
+counter = 0
+
+vehicle.mode = VehicleMode("RTL")
+#retourne au lauch
+
+
+
+
+
+
+#vehicle.mode = VehicleMode("LAND")
 time.sleep(2)
 
 while True:
